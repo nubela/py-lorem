c8695aac38fbf40eb98b83caef3cd74bc0f4be4b
nubela <nubela@ctrleff.com>
2013-03-31 02:08:01 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py-lorem',
+    version='1.0',
+    description='Generate mock sentences/paragraphs with the Lorem Ipsum prose',
+    url='https://github.com/nubela/py-lorem',
+    author='nubela',
+    author_email='nubela@gmail.com',
+    license='MIT',
+    packages=['loremipsum'],
+    zip_safe=False)
